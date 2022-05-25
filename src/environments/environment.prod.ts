513496081428a975fd603f2fd3edf297ed29823b
fabiocortesdev <fabiocortesdev@gmail.com>
2022-05-25 19:31:41 -0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'diario-viagens-fabio',
+    appId: '1:609852375082:web:5b05d4787ae451d8030353',
+    storageBucket: 'diario-viagens-fabio.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDwK3Uyv4GvbhF7QIzoPekhndHZKl8Lv4U',
+    authDomain: 'diario-viagens-fabio.firebaseapp.com',
+    messagingSenderId: '609852375082',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'diario-viagens-fabio',
+    appId: '1:609852375082:web:5b05d4787ae451d8030353',
+    storageBucket: 'diario-viagens-fabio.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDwK3Uyv4GvbhF7QIzoPekhndHZKl8Lv4U',
+    authDomain: 'diario-viagens-fabio.firebaseapp.com',
+    messagingSenderId: '609852375082',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
